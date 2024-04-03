11fe23954a20c6332108c4f3cd85f5921282183c
pawan-sharma3110 <pawansharma93520@gmail.com>
2024-04-04 00:22:02 +0530

    first task lower case complete

--- my-react-app/src/Components/Textform.js
+++ my-react-app/src/Components/Textform.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 
 export default function Textarea(props) {
+ 
   const handelUPCase = () => {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const handelLowCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
   const handelOnChanged = (event) => {
     setText(event.target.value);
   };
@@ -23,6 +29,13 @@ export default function Textarea(props) {
       </div>
       <button type="button" className="btn btn-primary" onClick={handelUPCase}>
         Conver Text To Upper Case
+      </button>
+      <button
+        type="button"
+        className="btn btn-primary lower-case"
+        onClick={handelLowCase}
+      >
+        Conver Text To Lower Case
       </button>
     </div>
   );
